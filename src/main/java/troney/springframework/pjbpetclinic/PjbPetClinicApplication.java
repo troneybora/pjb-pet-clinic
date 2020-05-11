fb565a66fdc21c3152f5f384c657760d6cba4073
Troney <troneybora@hotmail.com>
2020-05-11 17:58:15 +0530

    Initial Project Add

---
+++ src/main/java/troney/springframework/pjbpetclinic/PjbPetClinicApplication.java
@@ -0,0 +1,13 @@
+package troney.springframework.pjbpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjbPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PjbPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/troney/springframework/pjbpetclinic/PjbPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package troney.springframework.pjbpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjbPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
