fa6e08cfd5866bc988b1960f2cace16f6a65d2c4
Troney <troneybora@hotmail.com>
2020-05-15 14:42:15 +0530

    update pom.xml to include SNAPSHOT

--- pet-clinic-web/src/test/java/troney/springframework/pjbpetclinic/PjbPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/troney/springframework/pjbpetclinic/PjbPetClinicApplicationTests.java
@@ -1,9 +1,9 @@
-package pjbpetclinic;
+package troney.springframework.pjbpetclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {PjbPetClinicApplication.class})
 class PjbPetClinicApplicationTests {
 
     @Test
