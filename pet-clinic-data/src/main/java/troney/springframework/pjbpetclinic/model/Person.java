069f057a4da06b83071fc026e00ef9b5766fa19f
Troney <troneybora@hotmail.com>
2020-05-16 22:07:32 +0530

    Adding base entity. Closes #10

--- pet-clinic-data/src/main/java/troney/springframework/pjbpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/troney/springframework/pjbpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package troney.springframework.pjbpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/troney/springframework/pjbpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/troney/springframework/pjbpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package troney.springframework.pjbpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
