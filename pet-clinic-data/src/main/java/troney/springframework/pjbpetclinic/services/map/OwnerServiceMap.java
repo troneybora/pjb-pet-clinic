3bfdb032191abe2ffc4ff476cf6e8766525b3301
Troney <troneybora@hotmail.com>
2020-05-19 15:49:00 +0530

    Implemented Map based Service. Closes #6

---
+++ pet-clinic-data/src/main/java/troney/springframework/pjbpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,35 @@
+package troney.springframework.pjbpetclinic.services.map;
+
+import troney.springframework.pjbpetclinic.model.Owner;
+import troney.springframework.pjbpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner,Long> implements CrudService<Owner,Long> {
+
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+    super.deleteById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+    super.delete(object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
