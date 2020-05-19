f1c3fd97de86d7381a8b6edd3914f557d18959e6
Troney <troneybora@hotmail.com>
2020-05-19 15:22:11 +0530

    Refactoring to common Crud Interface. Closes #11

--- pet-clinic-data/src/main/java/troney/springframework/pjbpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/troney/springframework/pjbpetclinic/services/OwnerService.java
@@ -4,11 +4,9 @@ import troney.springframework.pjbpetclinic.model.Owner;
 
 import java.util.Set;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner,Long>{
 
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
+
 
 }
