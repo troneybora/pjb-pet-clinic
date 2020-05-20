0bdffdaaf1fd4f3f04ad79b02f3bfff40b5ef50c
Troney <troneybora@hotmail.com>
2020-05-20 14:01:01 +0530

    Added owner list and updated controller. Closes #16

--- pet-clinic-web/src/main/java/troney/springframework/pjbpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/troney/springframework/pjbpetclinic/controller/OwnerController.java
@@ -1,14 +1,26 @@
 package troney.springframework.pjbpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import troney.springframework.pjbpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+
+
     @RequestMapping({"","/", "/index", "/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
