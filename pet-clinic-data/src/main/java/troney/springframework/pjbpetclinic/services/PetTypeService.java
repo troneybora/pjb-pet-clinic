b0d127e61f55d900726f3f06dc68a6b01ccff707
Troney <troneybora@hotmail.com>
2020-05-21 19:40:27 +0530

    Added PetTypeService. Closes #30

---
+++ pet-clinic-data/src/main/java/troney/springframework/pjbpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package troney.springframework.pjbpetclinic.services;
+
+import troney.springframework.pjbpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
