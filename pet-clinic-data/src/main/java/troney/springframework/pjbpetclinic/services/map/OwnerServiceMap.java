37b71ecb598f354b5176c8dbba80b125f090fa6a
Troney <troneybora@hotmail.com>
2020-05-21 21:00:03 +0530

    Added Pets and Contact info to owner on startup. Closes #34

--- pet-clinic-data/src/main/java/troney/springframework/pjbpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/troney/springframework/pjbpetclinic/services/map/OwnerServiceMap.java
@@ -2,13 +2,23 @@ package troney.springframework.pjbpetclinic.services.map;
 
 import org.springframework.stereotype.Service;
 import troney.springframework.pjbpetclinic.model.Owner;
+import troney.springframework.pjbpetclinic.model.Pet;
 import troney.springframework.pjbpetclinic.services.OwnerService;
+import troney.springframework.pjbpetclinic.services.PetService;
+import troney.springframework.pjbpetclinic.services.PetTypeService;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner,Long> implements OwnerService {
 
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
 
     @Override
     public Set<Owner> findAll() {
@@ -22,7 +32,32 @@ public class OwnerServiceMap extends AbstractMapService<Owner,Long> implements O
 
     @Override
     public Owner save(Owner object) {
-        return super.save(object);
+        Owner savedOwner = null;
+
+        if(object != null){
+            if(object.getPets()!= null)
+            {
+                object.getPets().forEach(pet ->{if (pet.getPetType()!=null){
+                    if (pet.getPetType().getId()==null){
+                        pet.setPetType(petTypeService.save(pet.getPetType()));
+                    }
+                }else{
+                    throw new RuntimeException("Pet Type is required");
+                }
+                if (pet.getId()== null){
+                    Pet savedPet = petService.save(pet);
+                    pet.setId(savedPet.getId());
+                }
+                });
+            }
+            return super.save(object);
+
+        }
+        else{
+            return null;
+        }
+
+
     }
 
     @Override
--- pet-clinic-web/src/main/java/troney/springframework/pjbpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/troney/springframework/pjbpetclinic/bootstrap/DataLoader.java
@@ -3,11 +3,14 @@ package troney.springframework.pjbpetclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import troney.springframework.pjbpetclinic.model.Owner;
+import troney.springframework.pjbpetclinic.model.Pet;
 import troney.springframework.pjbpetclinic.model.PetType;
 import troney.springframework.pjbpetclinic.model.Vet;
 import troney.springframework.pjbpetclinic.services.OwnerService;
 import troney.springframework.pjbpetclinic.services.PetTypeService;
 import troney.springframework.pjbpetclinic.services.VetService;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -36,12 +39,33 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Troney");
         owner1.setLastName("Bora");
+        owner1.setAddress("Thane");
+        owner1.setCity("Mumbai");
+        owner1.setTelephone("914367895");
+
+        Pet troneyPet = new Pet();
+        troneyPet.setPetType(savedCatPetType);
+        troneyPet.setOwner(owner1);
+        troneyPet.setBirthDate(LocalDate.now());
+        troneyPet.setName("Just Cat");
+        owner1.getPets().add(troneyPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Prayash");
         owner2.setLastName("Bora");
+        owner2.setAddress("Thane");
+        owner2.setCity("Mumbai");
+        owner2.setTelephone("914367895");
+
+
+        Pet prayashPet = new Pet();
+        prayashPet.setPetType(savedDogPetType);
+        prayashPet.setOwner(owner2);
+        prayashPet.setBirthDate(LocalDate.now());
+        prayashPet.setName("Rosco");
+        owner2.getPets().add(prayashPet);
 
         ownerService.save(owner2);
 
