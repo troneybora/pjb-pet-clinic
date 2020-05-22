0a6dabb64af9d4f0a59c1836bed3a7380a08d43e
Troney <troneybora@hotmail.com>
2020-05-22 13:25:36 +0530

    Added Specialities Map Impl. Closes #32

---
+++ pet-clinic-data/src/main/java/troney/springframework/pjbpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,8 @@
+package troney.springframework.pjbpetclinic.services;
+
+import troney.springframework.pjbpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality,Long>{
+
+
+}
---
+++ pet-clinic-data/src/main/java/troney/springframework/pjbpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,34 @@
+package troney.springframework.pjbpetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import troney.springframework.pjbpetclinic.model.Speciality;
+import troney.springframework.pjbpetclinic.services.SpecialitiesService;
+
+import java.util.Set;
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality,Long> implements SpecialitiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+    super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+    super.deleteById(id);
+    }
+}
