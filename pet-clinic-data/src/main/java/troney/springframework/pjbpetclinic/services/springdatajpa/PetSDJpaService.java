6c30cae45fefc50aa059e2ea505cb68d1e232b08
Troney <troneybora@hotmail.com>
2020-05-25 18:17:25 +0530

    Added spring data JPA service for Pet. Closes #43

---
+++ pet-clinic-data/src/main/java/troney/springframework/pjbpetclinic/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,48 @@
+package troney.springframework.pjbpetclinic.services.springdatajpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import troney.springframework.pjbpetclinic.model.Pet;
+import troney.springframework.pjbpetclinic.repositories.PetRepository;
+import troney.springframework.pjbpetclinic.services.PetService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetSDJpaService implements PetService {
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> petSet = new HashSet<>();
+
+        petRepository.findAll().forEach(petSet::add);
+        return petSet;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
