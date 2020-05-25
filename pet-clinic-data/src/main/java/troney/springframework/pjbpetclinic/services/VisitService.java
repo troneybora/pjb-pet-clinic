086004a15772f55b0efe357fa8b47debd2b9ec92
Troney <troneybora@hotmail.com>
2020-05-25 18:58:34 +0530

    Implemented map Impl for Visit Service. Closes #47

---
+++ pet-clinic-data/src/main/java/troney/springframework/pjbpetclinic/services/VisitService.java
@@ -0,0 +1,8 @@
+package troney.springframework.pjbpetclinic.services;
+
+import troney.springframework.pjbpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit,Long>{
+
+
+}
